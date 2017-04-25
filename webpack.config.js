c8e78bdb27e961cc6a1cedf709655c9318108618
UVM1-59C77E1\vpipan <vlad.pipan@mhp.de>
2017-04-25 00:11:50 -0700

    changing port

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = function makeWebpackConfig() {
 
   config.output = {
     path: __dirname + '/dist',
-    publicPath: isProd ? '/' : 'http://localhost:9999/',
+    publicPath: isProd ? '/' : 'http://localhost:8080/',
     filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
     chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
   };
